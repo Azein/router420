aa1d3406f173aa690482325603cfbd26986692e1
Azein Pwr <azeinpwr@gmail.com>
2017-04-12 13:42:17 +0700

    Id matching fix

--- src/route.js
+++ src/route.js
@@ -6,7 +6,7 @@ export default class Route extends React.Component {
     if (this.props.location === this.props.pattern) {return <this.props.component />}
     if (this.props.pattern.includes(':id')){
       const routeId = this.props.location.substring(this.props.location.lastIndexOf('/')+1)
-      if (this.props.location === this.props.pattern.slice(0, -3)+'/'+routeId){
+      if (this.props.location === this.props.pattern.slice(0, -3)+routeId){
         return <this.props.component routeId={routeId} />
       }
     }
